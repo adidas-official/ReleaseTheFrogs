75f073ab34486769c9daa5270b410ec590132bf7
adidas-official <blueman11235@gmail.com>
2022-01-17 19:53:30 +0100

    - creating and renaming form for current month

--- sheets.py
+++ sheets.py
@@ -7,7 +7,7 @@ import functions
 import time
 
 logging.basicConfig(
-    # filename='run.log',
+    filename='run.log',
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S')
